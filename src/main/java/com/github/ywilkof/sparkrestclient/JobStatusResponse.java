b24eeb5a6e20b468cbb009ed38af8b495a0cffd2
yonatan <ywilkof@gmail.com>
2017-05-21 12:33:36 +0200

    accessors for worker id and host should be optional

--- src/main/java/com/github/ywilkof/sparkrestclient/JobStatusResponse.java
+++ src/main/java/com/github/ywilkof/sparkrestclient/JobStatusResponse.java
@@ -4,10 +4,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Optional;
+
 /**
  * Created by Yuval.Itzchakov on 30/01/2017.
  */
-@Getter
 @Setter(AccessLevel.PACKAGE)
 public class JobStatusResponse extends SparkResponse {
 
@@ -16,13 +17,22 @@ public class JobStatusResponse extends SparkResponse {
      * Format: "[worker-ip]:[worker-port]"
      */
     private String workerHostPort;
+
+    public Optional<String> getWorkerHostPort() {
+        return Optional.ofNullable(workerHostPort);
+    }
+
     /**
      * The worker ID as assigned by Spark.
      */
     private String workerId;
 
+    public Optional<String> getWorkerId() {
+        return Optional.ofNullable(workerId);
+    }
+
     /**
      * Reports the status of the driver.
      */
-    private DriverState driverState;
+    @Getter private DriverState driverState;
 }
